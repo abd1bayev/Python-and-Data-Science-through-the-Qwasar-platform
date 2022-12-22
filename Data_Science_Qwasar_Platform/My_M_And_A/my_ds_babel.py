246757ff42df17162760e81767693c4a35c47c0f
jasur <jasurabdibayev0@gmail.com>
2022-12-22 10:53:11 +0500

    My M And A db

---
+++ Data_Science_Qwasar_Platform/My_M_And_A/my_ds_babel.py
@@ -0,0 +1,17 @@
+import sqlite3
+import pandas as pd
+from my_m_and_a import table_1,table_2,table_3
+
+def main():
+    all = pd.concat([table_1, table_2, table_3])
+    connection = sqlite3.connect("All_data.db")
+    cursor = connection.cursor()
+    cursor.execute(f'CREATE TABLE IF NOT EXISTS customers(gender text, firstname text, lastname text, email text, age integer, city text, country text, created_at text, referral text)')
+    connection.commit()
+    all.to_sql('customers', connection, if_exists='replace', index = False)
+    cursor.close()
+    connection.close()
+    #
+    # print(all)
+
+main()
