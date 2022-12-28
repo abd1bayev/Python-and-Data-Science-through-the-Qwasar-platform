6f9e1089b8e4d64167b6c9bfbb704717a7a57c16
student <jasurabdibayev0@gmail.com>
2022-12-28 12:09:52 +0500

    exercises

---
+++ Data_Science_Qwasar_Platform/Exercises/my_each.py
@@ -0,0 +1,5 @@
+def my_each(param_1):
+    for i in param_1:
+        return i
+
+print(my_each(["blah1", "blah2", "blah3"]))
---
+++ Data_Science_Qwasar_Platform/Exercises/my_recursive_pow.py
@@ -0,0 +1,5 @@
+def my_recursive_pow(param_1, param_2):
+	qwe = int(param_1)**int(param_2)
+	return qwe
+
+print(my_recursive_pow(2, 3))
