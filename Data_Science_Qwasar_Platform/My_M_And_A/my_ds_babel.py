17578e9c1d16c725fb3d5ae3184fa9d23df514d6
student <jasurabdibayev0@gmail.com>
2023-05-25 16:31:19 +0500

    My_M_and_A update

--- Data_Science_Qwasar_Platform/My_M_And_A/my_ds_babel.py
+++ Data_Science_Qwasar_Platform/My_M_And_A/my_ds_babel.py
@@ -1,17 +1,27 @@
 import sqlite3
+import csv
 import pandas as pd
-from my_m_and_a import table_1,table_2,table_3
 
-def main():
-    all = pd.concat([table_1, table_2, table_3])
-    connection = sqlite3.connect("All_data.db")
+
+def sql_to_csv(database, table_name):
+    connection = sqlite3.connect(database)
     cursor = connection.cursor()
-    cursor.execute(f'CREATE TABLE IF NOT EXISTS customers(gender text, firstname text, lastname text, email text, age integer, city text, country text, created_at text, referral text)')
-    connection.commit()
-    all.to_sql('customers', connection, if_exists='replace', index = False)
-    cursor.close()
-    connection.close()
-    #
-    # print(all)
+    cursor.execute('SELECT * FROM ' + table_name)
+    result_1 = cursor.fetchall()
+    header = [i[0].title().replace("_", " ") for i in cursor.description]
+    l = []
+    l.append(','.join(header))
+    for tup in result_1:
+        l.append(','.join([str(i) for i in tup]))
+    final_res = "\n".join(l)
+    return final_res
 
-main()
+
+
+def csv_to_sql(csv_content, database, table_name):
+    a = ['Mega Basalt Field', 'Ethiopia']
+    df = pd.read_csv(csv_content)
+    if database == 'list_volcanos.db':
+        df = df[~df['Volcano Name'].isin(a)]
+    connection = sqlite3.connect(database)
+    df.to_sql(table_name, con=connection, if_exists='replace', index=False)
--- Data_Science_Qwasar_Platform/My_M_And_A/my_m_and_a.py
+++ Data_Science_Qwasar_Platform/My_M_And_A/my_m_and_a.py
@@ -1,71 +1,83 @@
+import numpy as np
+import csv
+import my_ds_babel
+from io import StringIO
 import pandas as pd
-
-def table_1():
-    table_1 = pd.read_csv("Data/only_wood_customer_us_1.csv")
-    table_1 = table_1[["Gender","FirstName", "LastName", "Email", "Age", "City", "Country"]]
-    gender = {'0':'Male', '1':'Female','M': 'Male','F': 'Female'}
-    table_1['Gender'] = table_1['Gender'].replace(gender)
-    table_1['Country'] = 'USA'
-    table_1["Email"] = table_1["Email"].str.lower()
-    table_1['FirstName'] = table_1['FirstName'].str.capitalize()
-    table_1['LastName'] = table_1['LastName'].str.capitalize()
-    table_1['City'] = table_1['City'].str.replace('_',' ')
-    table_1['City'] = table_1['City'].str.replace('-',' ')
-    table_1['FirstName'] = table_1['FirstName'].str.replace('\\','')
-    table_1['LastName'] = table_1['LastName'].str.replace('\\','')
-    table_1['FirstName'] = table_1['FirstName'].str.replace('"','')
-    table_1['LastName'] = table_1['LastName'].str.replace('"','')
-    table_1['City'] = table_1['City'].str.title()
-
-    return table_1
-
-table_1 = table_1()
-
-def table_2():
-    table_2 = pd.read_csv("Data/only_wood_customer_us_2.csv", sep =';', header = None, names = ['Age', 'City', 'Gender', 'Name', 'Email'])
-    table_2[['FirstName','LastName']] = table_2.Name.str.split(expand=True)
-    table_2.drop('Name', inplace=True, axis=1)
-    table_2['City'] = table_2['City'].str.replace('_',' ').replace('-',' ')
-    # table_2['City'] = table_2['City'].str.replace('-',' ')
-    table_2['City'] = table_2['City'].str.title()
-    gender = {'0':'Male', '1':'Female','M': 'Male','F': 'Female'}
-    table_2['Gender'] = table_2['Gender'].replace(gender)
-    table_2['Country'] = 'USA'
-    table_2["Email"] = table_2["Email"].str.lower()
-    table_2['FirstName'] = table_2['FirstName'].str.replace('\\','').str.replace('"','').str.capitalize()
-    table_2['LastName'] = table_2['LastName'].str.replace('\\','').str.replace('"','').str.capitalize()
-    table_2 = table_2[["Gender","FirstName", "LastName", "Email", "Age", "City", "Country"]]
-
-    return table_2
-    # print(table_2)
-
-table_2 = table_2()
-
-def table_3():
-    table_3 = pd.read_csv("Data/only_wood_customer_us_3.csv", delimiter='\t',
-                          names=['Gender', 'Name', 'Email', 'Age', 'City', 'Country'],
-                          header=0, na_values=['N/A'])
-    table_3[['FirstName', 'LastName']] = table_3.Name.str.split(expand=True)
-    table_3.drop('Name', inplace=True, axis=1)
-    table_3['City'] = table_3['City'].str.replace('_', ' ')
-    table_3['City'] = table_3['City'].str.replace('-', ' ')
-    table_3['City'] = table_3['City'].str.title()
-    table_3 = table_3.replace(to_replace=r'string_|integer_|boolean_|character_|String', value='', regex=True)
-    table_3['Age'] = table_3['Age'].replace(to_replace=r'[a-zA-Z]', value='', regex=True)
-    gender = {'0': 'Male', '1': 'Female', 'M': 'Male', 'F': 'Female'}
-    Gender = table_3['Gender'] = table_3['Gender'].replace(gender)
-    table_3['Country'] = 'USA'
-    table_3["Email"] = table_3["Email"].str.lower()
-    table_3['FirstName'] = table_3['FirstName'].str.replace('\\', '')
-    table_3['LastName'] = table_3['LastName'].str.replace('\\', '')
-    table_3['FirstName'] = table_3['FirstName'].str.replace('"', '')
-    table_3['LastName'] = table_3['LastName'].str.replace('"', '')
-    table_3['FirstName'] = table_3['FirstName'].str.capitalize()
-    table_3['LastName'] = table_3['LastName'].str.capitalize()
-    table_3 = table_3[["Gender", "FirstName", "LastName", "Email", "Age", "City", "Country"]]
-    # print(table_3)
-    return table_3
-
-table_3 = table_3()
+from warnings import filterwarnings
+filterwarnings("ignore")
 
 
+def first_ds(param_1):
+    if len(param_1.getvalue()) > len('/Users/student/Desktop/Kerakli/Project all/Django/Python-and-Data-Science-through-the-Qwasar-platform/Data_Science_Qwasar_Platform/My_M_And_A/Data/only_wood_customer_us_1.csv'):
+        df_1_raw = pd.read_csv(param_1)
+    else:
+        df_1_raw = pd.read_csv(param_1.getvalue())
+    df_1_raw.replace({'Gender':{'0':'Male', '1':'Female', 'M':'Male', 'F':'Female'}}, inplace=True)
+    df_1_raw['FirstName'] = df_1_raw['FirstName'].str.title().replace('[^a-zA-Z]', '', regex=True)
+    df_1_raw['LastName'] = df_1_raw['LastName'].str.title().replace('[^a-zA-Z]', '', regex=True)
+    df_1_raw['Email'] = df_1_raw['Email'].str.lower()
+    df_1_raw['City'] = df_1_raw['City'].str.title().replace('[^a-zA-Z]', ' ', regex=True)
+    df_1_raw['Country'] = 'USA'
+    df_1_raw.replace({'Email':{'forgottoask@woodinc':np.nan}}, inplace=True)
+    df_1 = df_1_raw.reindex(columns=['Gender', 'FirstName', 'LastName', 'UserName', 'Email', 'Age', 'City', 'Country'])
+    df_1 = df_1.astype(str)
+    return df_1
+
+
+def second_ds(param_1):
+    column_names = ['Age', 'City', 'Gender', 'FullName', 'Email']
+    if len(param_1.getvalue()) > len('/Users/student/Desktop/Kerakli/Project all/Django/Python-and-Data-Science-through-the-Qwasar-platform/Data_Science_Qwasar_Platform/My_M_And_A/Data/only_wood_customer_us_2.csv'):
+        df_2_raw = pd.read_csv(param_1,  sep=';', names=column_names)
+    else:
+        df_2_raw = pd.read_csv(param_1.getvalue(),  sep=';', names=column_names)
+    df_2_raw['Age'] = df_2_raw['Age'].replace('[^0-9]', '', regex=True)
+    df_2_raw['City'] = df_2_raw['City'].str.title().replace('[^a-zA-Z]', ' ', regex=True)
+    df_2_raw['Email'] = df_2_raw['Email'].str.lower()
+    df_2_raw.replace({'Gender':{'0':'Male', '1':'Female', 'M':'Male', 'F':'Female'}}, inplace=True)
+    df_2_raw[['FirstName', 'LastName']] = df_2_raw['FullName'].str.title().replace('[^a-zA-Z- ]', '', regex=True).str.extract(r'(\w+) (\w+)', expand=True) #.str.split(' ', 1, expand=True)
+    df_2_raw.drop('FullName', inplace=True, axis=1)
+    df_2_raw['UserName'] = df_2_raw['FirstName'].str.lower()
+    df_2_raw['Country'] = 'USA'
+    df_2 = df_2_raw.reindex(columns=['Gender', 'FirstName', 'LastName', 'UserName', 'Email', 'Age', 'City', 'Country'])
+    df_2 = df_2.astype(str)
+    return df_2
+
+
+def third_ds(param_1):
+    if len(param_1.getvalue()) > len('/Users/student/Desktop/Kerakli/Project all/Django/Python-and-Data-Science-through-the-Qwasar-platform/Data_Science_Qwasar_Platform/My_M_And_A/Data/only_wood_customer_us_3.csv'):
+        filename = (param_1.getvalue()).split('\n')
+        lst = []
+        for row in filename:
+            lst.append(row.split('\t'))
+    else:
+        filename = param_1.getvalue()
+        lst = []
+        with open(filename, 'r') as csvfile:
+            datareader = csv.reader(csvfile)
+            for row in datareader:
+                lst.append(row[0].split('\t'))
+    lst.pop(0)
+    df_3_raw = pd.DataFrame(lst, columns =['Gender', 'Name', 'Email', 'Age', 'City', 'Country'])
+    df_3_raw.replace({'Gender':{'boolean_0':'Male', 'boolean_1':'Female', 'character_M':'Male', 'string_Female':'Female', 'string_Male':'Male'}}, inplace=True)
+    df_3_raw['Age'] = df_3_raw['Age'].replace('[^0-9]', '', regex=True)
+    df_3_raw[['FirstName', 'LastName']] = df_3_raw['Name'].str.title().replace('(^String)', '', regex=True).replace('[^a-zA-Z- ]', '', regex=True).str.extract(r'(\w+) (\w+)', expand=True)
+    df_3_raw.drop('Name', inplace=True, axis=1)
+    df_3_raw['City'] = df_3_raw['City'].str.title().replace('(^String)', '', regex=True).replace('[^a-zA-Z]', ' ', regex=True)
+    df_3_raw['Email'] = df_3_raw['Email'].str.lower().replace('(^string.)', '', regex=True)
+    df_3_raw.replace({'Email':{'n/a':np.nan}}, inplace=True)
+    df_3_raw['UserName'] = df_3_raw['FirstName'].str.lower()
+    df_3_raw['Country'] = 'USA'
+    df_3 = df_3_raw.reindex(columns=['Gender', 'FirstName', 'LastName', 'UserName', 'Email', 'Age', 'City', 'Country'])
+    df_3 = df_3.astype(str)
+    return df_3
+
+
+def my_m_and_a(dataset_1, dataset_2, dataset_3):
+    frames = [first_ds(dataset_1), second_ds(dataset_2), third_ds(dataset_3)]
+    result = pd.concat(frames)
+    return pd.DataFrame(result)
+
+
+if __name__ == '__main__':
+    merged_csv = my_m_and_a(StringIO('/Users/student/Desktop/Kerakli/Project all/Django/Python-and-Data-Science-through-the-Qwasar-platform/Data_Science_Qwasar_Platform/My_M_And_A/Data/only_wood_customer_us_1.csv'), StringIO('/Users/student/Desktop/Kerakli/Project all/Django/Python-and-Data-Science-through-the-Qwasar-platform/Data_Science_Qwasar_Platform/My_M_And_A/Data/only_wood_customer_us_2.csv'), StringIO('/Users/student/Desktop/Kerakli/Project all/Django/Python-and-Data-Science-through-the-Qwasar-platform/Data_Science_Qwasar_Platform/My_M_And_A/Data/only_wood_customer_us_3.csv')).to_csv(index=False)
+    my_ds_babel.csv_to_sql(StringIO(merged_csv), 'plastic_free_boutique.db', 'customers')
